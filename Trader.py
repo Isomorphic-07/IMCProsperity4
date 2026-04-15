dd3f964fcbf4571cb7f72f027f95bbfbd163e430
Wimers <113898263+Wimers@users.noreply.github.com>
2026-04-15 22:35:07 +1000

    Added data model code, and Trader class, where we will implement the algo.

--- Trader.py
+++ Trader.py
@@ -1,81 +1,12 @@
-from typing import List, Dict
-import string
-import json
-
-# Implement
-# -------------------------
-# per product position limits 
-
-
-class ConversionObservation:
-
-    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sunlight: float, humidity: float):
-        self.bidPrice = bidPrice
-        self.askPrice = askPrice
-        self.transportFees = transportFees
-        self.exportTariff = exportTariff
-        self.importTariff = importTariff
-        self.sugarPrice = sugarPrice
-        self.sunlightIndex = sunlightIndex
-
-class TradingState(object):
-   def __init__(self,
-                 traderData: str,
-                 timestamp: Time,
-                 listings: Dict[Symbol, Listing],
-                 order_depths: Dict[Symbol, OrderDepth],
-                 own_trades: Dict[Symbol, List[Trade]],
-                 market_trades: Dict[Symbol, List[Trade]],
-                 position: Dict[Product, Position],
-                 observations: Observation):
-        self.traderData = traderData
-        self.timestamp = timestamp
-        self.listings = listings
-        self.order_depths = order_depths
-        self.own_trades = own_trades
-        self.market_trades = market_trades
-        self.position = position
-        self.observations = observations
-        
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
-
+from datamodel import Listing, ConversionObservation, Observation, Order, OrderDepth, Trade, TradingState, ProsperityEncoder
 
 class Trader:
     def __init__():
         pass
-    
+
     def run(self, state: TradingState):
         pass
 
     # Ignore the following code for, now. Will be implemented in the future for Round 2
     def bid(self):
         pass
-
-Time = int
-Symbol = str
-Product = str
-Position = int
-   
-Symbol = str
-UserId = str
-
-class Trade:
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None, timestamp: int = 0) -> None:
-        self.symbol = symbol
-        self.price: int = price
-        self.quantity: int = quantity
-        self.buyer = buyer
-        self.seller = seller
-        self.timestamp = timestamp
-
-    def __str__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
-
-    def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
-    
-class OrderDepth:
-    def __init__(self):
-        self.buy_orders: Dict[int, int] = {}
-        self.sell_orders: Dict[int, int] = {}
