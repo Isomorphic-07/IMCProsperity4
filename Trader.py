2f6363d292e58500db79f3f3fdca2fa6c6c2e9ae
Wimers <113898263+Wimers@users.noreply.github.com>
2026-04-16 19:22:45 +1000

    Added basic functionality to find the best prices to buy / sell at based on incoming orders.

--- Trader.py
+++ Trader.py
@@ -7,10 +7,17 @@ from datamodel import (
     ProsperityEncoder
 )
 
+ashCoatedOsmiumLimit = 80  # ASH_COATED_OSMIUM
+intarianPepperRootLimit = 80  # INTARIAN_PEPPER_ROOT
+
+BUY = 1
+SELL = -1
+
 
 class Trader:
-    def __init__():
-        pass
+    def __init__(self) -> None:
+        self.ashCoatedOsmiumPosition = 0
+        self.intarianPepperRootPosition = 0
 
     # This is the main function called every round, where the logic for
     # our algorithm will be impemented.
@@ -19,4 +26,36 @@ class Trader:
 
     # Ignore this class for now, only required for Round 2 and beyond.
     def bid(self):
+        pass
+
+    # Ignore our own trades, only consider trades from other traders.
+    def is_own_trade(self, trade: Trade) -> bool:
+        return (trade.get_buyer() == "SUBMISSION") or (trade.get_seller() == "SUBMISSION")
+
+    def get_max_position(self, symbol: str, direction: int) -> int:
+
+        # Confirm direction is valid
+        if (direction != BUY and direction != SELL):
+            raise ValueError("Direction must be either BUY or SELL")
+
+        # Confirm symbol is valid
+        if (symbol != "ASH_COATED_OSMIUM" and symbol != "INTARIAN_PEPPER_ROOT"):
+            raise ValueError("Unknown symbol: " + symbol)
+
+        if (symbol == "ASH_COATED_OSMIUM"):
+            return ashCoatedOsmiumLimit - (
+                direction * self.ashCoatedOsmiumPosition
+            )
+
+        if (symbol == "INTARIAN_PEPPER_ROOT"):
+            return intarianPepperRootLimit - (
+                direction * self.intarianPepperRootPosition
+            )
+
+    def calculate_fair_price(self, order_depth: OrderDepth) -> int:
+        '''
+        Could just take the mid price, from the best buy and sell orders.
+        But if there are more orders on one side, we might want to weight
+        the price more towards that side.
+        '''
         pass
--- datamodel.py
+++ datamodel.py
@@ -21,7 +21,17 @@ class Listing:
 
 class ConversionObservation:
 
-    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sunlight: float, humidity: float):
+    def __init__(
+            self,
+            bidPrice: float,
+            askPrice: float,
+            transportFees: float,
+            exportTariff: float,
+            importTariff: float,
+            sunlight: float,
+            humidity: float
+    ):
+
         self.bidPrice = bidPrice
         self.askPrice = askPrice
         self.transportFees = transportFees
@@ -30,14 +40,13 @@ class ConversionObservation:
         self.sugarPrice = sugarPrice
         self.sunlightIndex = sunlightIndex
 
-
 class Observation:
 
     def __init__(
             self,
             plainValueObservations: Dict[Product, ObservationValue],
             conversionObservations: Dict[Product, ConversionObservation]
-        ) -> None:
+    ) -> None:
 
         self.plainValueObservations = plainValueObservations
         self.conversionObservations = conversionObservations
@@ -66,6 +75,27 @@ class OrderDepth:
         self.buy_orders: Dict[int, int] = {}
         self.sell_orders: Dict[int, int] = {}
 
+    def sort_buys(self):
+        '''
+        Obviously, the best buy orders are the ones with the highest price,
+        so we want to sort them in decreasing order of price.
+        '''
+        self.buy_orders = dict(
+            sorted(self.buy_orders.items(),
+            key=lambda item: item[0],
+            reverse=True)
+        )
+
+    def sort_sells(self):
+        '''
+        The best sell orders are the ones with the lowest price,
+        so we want to sort them in increasing order of price.
+        '''
+        self.sell_orders = dict(
+            sorted(self.sell_orders.items(),
+            key=lambda item: item[0])
+        )
+
 
 class Trade:
 
@@ -92,6 +122,11 @@ class Trade:
     def __repr__(self) -> str:
         return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
 
+    def get_buyer(self) -> UserId:
+        return self.buyer
+
+    def get_seller(self) -> UserId:
+        return self.seller
 
 class TradingState(object):
 
@@ -107,10 +142,27 @@ class TradingState(object):
         self.traderData = traderData
         self.timestamp = timestamp
         self.listings = listings
+
+        # All the buy and sell orders per product that other market participants have sent
+        # and that the algorithm is able to trade with. This property is a dict where the
+        # keys are the products and the corresponding values are instances of the OrderDepth class.
+        # This OrderDepth class then contains all the buy and sell orders.
         self.order_depths = order_depths
+
+        # The trades the algorithm itself has done since the last TradingState came in.
+        # This property is a dictionary of Trade objects with key being a product name.
+        # The definition of the Trade class is provided in the subsections below.
         self.own_trades = own_trades
+
+        # The trades that other market participants have done since the last TradingState came in.
+        # This property is also a dictionary of Trade objects with key being a product name.
         self.market_trades = market_trades
+
+        # The long or short position that the player holds in every tradable product.
+        # This property is a dictionary with the product as the key for which the value is
+        # a signed integer denoting the position, e.g. {product1: 2, product2: -1}.
         self.position = position
+
         self.observations = observations
 
     def toJSON(self):
